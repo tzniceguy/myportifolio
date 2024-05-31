79a98a71597a67022bda12c6b57a4574250e30ad
tzniceguy <joeshimbi@gmail.com>
2024-05-31 17:08:04 +0300

    Introduction component

---
+++ app/components/Introduction.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Introduction() {
+  return (
+    <div className="gap-6">
+      <h2 className="text-xl">Hello, I am Joe from Tanzania</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        autem inventore quam voluptas fugiat illo blanditiis error distinctio
+        ipsam quisquam ut dignissimos nesciunt quibusdam, rerum aperiam! Dicta
+        repellendus autem a!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, hic
+        sit omnis in consequuntur corporis repudiandae repellat quam, ratione
+        dignissimos excepturi laborum voluptates obcaecati alias consectetur
+        iure voluptatibus, quod nemo.
+      </p>
+    </div>
+  );
+}
