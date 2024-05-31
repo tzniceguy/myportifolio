17a0e638adeb5d58db388614beb6017f249229a4
tzniceguy <joeshimbi@gmail.com>
2024-05-31 15:45:31 +0300

    change font from inter to space grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const space = Space_Grotesk({ subsets: ["latin"] });
+// <uniquifier>: Use a unique and descriptive class name
+// <weight>: Use a value from 300 to 700
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={space.className}>{children}</body>
     </html>
   );
 }
