df7bc3844fa6ad9d6e1dbbb7cd733e97a9845714
tzniceguy <joeshimbi@gmail.com>
2024-05-31 17:07:51 +0300

    products shiped component

---
+++ app/components/Showcase.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Project from "./Project";
+
+export default function Showcase() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold">My Works</h2>
+      <h2>Here is the list projects with my footprints</h2>
+      <div>
+        <Project />
+      </div>
+    </div>
+  );
+}
