e9974fd0d26f3cbd6c32d7ed09f9bdbe59de7fa8
tzniceguy <joeshimbi@gmail.com>
2024-05-31 17:06:27 +0300

    typescript to feed shiped products with my footprints

---
+++ app/data.ts
@@ -0,0 +1,26 @@
+export const build = [
+  {
+    title: "Future Allied",
+    date: "March 2024",
+    description: "lorem",
+    link: "/",
+  },
+  {
+    title: "Future Allied",
+    date: "March 2024",
+    description: "lorem",
+    link: "/",
+  },
+  {
+    title: "Future Allied",
+    date: "March 2024",
+    description: "lorem",
+    link: "/",
+  },
+  {
+    title: "Future Allied",
+    date: "March 2024",
+    description: "lorem",
+    link: "/",
+  },
+];
