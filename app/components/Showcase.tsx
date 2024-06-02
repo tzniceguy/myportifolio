1903ef34da94b16b3904bd65fdb35542b37b03cc
tzniceguy <joeshimbi@gmail.com>
2024-06-02 19:54:28 +0300

    added utility classes for the component

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -3,7 +3,7 @@ import Project from "./Project";
 
 export default function Showcase() {
   return (
-    <div>
+    <div className="space-y-4 border-b-2 m-2">
       <h2 className="text-xl font-semibold">My Works</h2>
       <h2>Here is the list projects with my footprints</h2>
       <div>
