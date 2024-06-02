3a0c65aaec5bbf05c9da0626960767bf8b4bcbfc
tzniceguy <joeshimbi@gmail.com>
2024-06-02 19:53:22 +0300

    renamed the webpage url

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-4 px-6 py-12 md:px-72">
+    <main className="flex min-h-screen flex-col gap-4 px-6 py-12 ">
       <div className="gap-4">
         <div>
           <h1 className="font-semibold text-2xl">about</h1>
