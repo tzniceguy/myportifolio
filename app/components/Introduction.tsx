a98887217cb45050ad2a639697f2beff57297b9b
tzniceguy <joeshimbi@gmail.com>
2024-06-02 19:42:32 +0300

    added some spacing

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -2,20 +2,16 @@ import React from "react";
 
 export default function Introduction() {
   return (
-    <div className="gap-6">
+    <div className="gap-6 space-y-4 border-b-2 m-2">
       <h2 className="text-xl">Hello, I am Joe from Tanzania</h2>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        autem inventore quam voluptas fugiat illo blanditiis error distinctio
-        ipsam quisquam ut dignissimos nesciunt quibusdam, rerum aperiam! Dicta
-        repellendus autem a!
+      <p className="">
+        A <strong>software Developer</strong>, interested in building intristic
+        backend puzzles and crafting elegant frontend for web applications.
       </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, hic
-        sit omnis in consequuntur corporis repudiandae repellat quam, ratione
-        dignissimos excepturi laborum voluptates obcaecati alias consectetur
-        iure voluptatibus, quod nemo.
+      <p className="">
+        I have been keeping with the technological advancement by learning best
+        and latest technologies. Feel free to check around and contact me
+        wherever you find me useful
       </p>
     </div>
   );
