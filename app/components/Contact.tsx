50f6f514bffc193193020a4f5bd36cc33dd88903
tzniceguy <joeshimbi@gmail.com>
2024-06-27 11:11:38 +0300

    added socials and github

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -7,8 +7,16 @@ export default function Contact() {
       <h2 className="text-xl font-semibold">Connect</h2>
       <ul>
         <li>
-          Email:{" "}
+          <span className="font-semibold">Email:</span>
           <Link href="mailto:joeshimbi@gmail.com">joeshimbi@gmail.com</Link>
+        </li>
+        <li>
+          <span className="font-semibold">X, Formely Twitter:</span>
+          <Link href="x.com/tzniceguy">@tzniceguy</Link>
+        </li>
+        <li>
+          <span className="font-semibold">Github:</span>
+          <Link href="github.com/tzniceguy">@tzniceguy</Link>
         </li>
       </ul>
     </div>
