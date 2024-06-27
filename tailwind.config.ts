e62cba7039b895c7b68770ce7c057a59f461c930
tzniceguy <joeshimbi@gmail.com>
2024-06-28 01:23:12 +0300

    added custom color shade to the tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        marshland: {
+          "50": "#f6f7f6",
+          "100": "#e3e5e2",
+          "200": "#c6cac5",
+          "300": "#a2a7a1",
+          "400": "#7e847d",
+          "500": "#636a62",
+          "600": "#4e544d",
+          "700": "#414540",
+          "800": "#363936",
+          "900": "#2f322f",
+          "950": "#191b19",
+        },
+      },
     },
   },
   plugins: [],
