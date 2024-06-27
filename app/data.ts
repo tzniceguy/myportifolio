4be11f3ab1f3bd9ae05540a44c365232c33d5291
tzniceguy <joeshimbi@gmail.com>
2024-06-27 11:11:57 +0300

    added projects with my footprints

--- app/data.ts
+++ app/data.ts
@@ -1,26 +1,33 @@
 export const build = [
   {
-    title: "Future Allied",
-    date: "March 2024",
-    description: "lorem",
+    title: "30daysfrontenddevelopment",
+    description:
+      "This repository chronicles my journey through a 30-day challenge focused on building user interfaces using Next.js and React. Each day, I will create a new UI component or page inspired by online design resources",
+    link: "https://30daysfrontenddevelopment.vercel.app/",
+  },
+  {
+    title: "Future Allied Website",
+    description:
+      "Website for a Tanzanian Registered company. The company has been offering several services to mining sector, Government Authorities and Private companies.",
+    link: "https://futureallied.co.tz",
+  },
+  {
+    title: "Lugumya",
+    description:
+      "Lugumya Expense Tracker & Budgeting Web App: A user-friendly solution for personal finance management, empowering better expense tracking, budget control, and informed financial decisions.",
     link: "/",
   },
   {
-    title: "Future Allied",
-    date: "March 2024",
-    description: "lorem",
-    link: "/",
+    title: "Shopika",
+    description:
+      "an ecommerce website made with Python Django framework and frontend crafted with html and tailwind css",
+    link: "/b",
   },
+  { title: "EDITH", description: "", link: "/", image: "/projectimage.jpg" },
   {
-    title: "Future Allied",
-    date: "March 2024",
-    description: "lorem",
-    link: "/",
-  },
-  {
-    title: "Future Allied",
-    date: "March 2024",
-    description: "lorem",
+    title: "candSqlLite",
+    description:
+      "A semister project i made in Data insertion and retrieval from database using C and Sqlite Database",
     link: "/",
   },
 ];
