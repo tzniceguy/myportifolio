b383fee25b606b1300c3448346f8fe60846bdadd
tzniceguy <joeshimbi@gmail.com>
2024-06-28 10:45:43 +0300

    styling the link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
 const navbaritems = [
   { title: "about", href: "/" },
   { title: "services", href: "/work" },
   { title: "blog", href: "/blog" },
 ];
 export default function NavBar() {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-6">
       <ul className="flex space-x-6 justify-end">
-        {navbaritems.map((item) => (
-          <li key={item.title}>
-            <Link href={item.href}>{item.title}</Link>
+        {navbaritems.map((link) => (
+          <li key={link.title}>
+            <Link
+              href={link.href}
+              className={clsx("underline", {
+                "no-underline text-marshland-300": pathname === link.href,
+              })}
+            >
+              {link.title}
+            </Link>
           </li>
         ))}
       </ul>
