61c416b1831907179c476821fc63a89363449403
tzniceguy <joeshimbi@gmail.com>
2024-06-28 10:46:23 +0300

    modied font size to medium screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${space.className} flex min-h-screen flex-col gap-4 md:px-72  dark:bg-marshland-950`}
+        className={`${space.className} flex min-h-screen flex-col gap-4 md:px-72 md:text-xl dark:bg-marshland-950`}
       >
         <ThemeProvider
           attribute="class"
