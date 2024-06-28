adb829031bb80367707be2355fc1161805ed3d31
tzniceguy <joeshimbi@gmail.com>
2024-06-28 10:44:41 +0300

    removed date

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -6,13 +6,16 @@ export default function Project() {
   return (
     <div className="space-y-6">
       {build.map((build) => (
-        <div key={build.title}>
-          <h2>{build.title}</h2>
-          <h2>{build.date}</h2>
+        <div key={build.title} className="pb-2 space-y-3">
+          <h2 className="font-semibold">{build.title}</h2>
           <p>{build.description}</p>
-          <span>
-            <Link href={build.link}>Visit</Link>
-          </span>
+          <p>
+            <span>
+              <Link href={build.link} className="underline">
+                Visit
+              </Link>
+            </span>
+          </p>
         </div>
       ))}
     </div>
