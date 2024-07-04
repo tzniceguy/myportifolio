f0835d27be3c4998b7ce84b240241239d26098bb
tzniceguy <joeshimbi@gmail.com>
2024-07-04 23:11:33 +0300

    spacing to table

--- app/components/blogtable.tsx
+++ app/components/blogtable.tsx
@@ -3,13 +3,15 @@ import React from "react";
 export default function BlogTable() {
   return (
     <div>
-      <table className="border-spacing-x-2">
+      <table className="w-full table-auto text-sm  md:text-md text-left rtl:text-right">
         <thead>
           <tr>
             <th>date</th>
             <th>title</th>
           </tr>
-          <tr>
+        </thead>
+        <tbody>
+          <tr className="border-b hover:bg-marshland-500">
             <td>2024</td>
             <td>journey towards front end development with nextjs</td>
           </tr>
@@ -17,7 +19,7 @@ export default function BlogTable() {
             <td>2024</td>
             <td>Boy Alone</td>
           </tr>
-        </thead>
+        </tbody>
       </table>
     </div>
   );
