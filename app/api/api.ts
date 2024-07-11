143cab1760db45f2a0f80a41178d5a6e446532b9
tzniceguy <joeshimbi@gmail.com>
2024-07-11 08:35:07 +0300

    added dynamic route for each url

--- app/api/api.ts
+++ app/api/api.ts
@@ -8,14 +8,14 @@ export const fetchPosts = async () => {
     const response = await axios.get(`${API_URL}`);
     return response.data;
   } catch (error) {
-    console.error("error fetching items:", error);
+    console.error("error fetching posts:", error);
     throw error;
   }
 };
 
 export const fetchPostById = async (id: number): Promise<Post> => {
   try {
-    const response = await axios.get(`${API_URL}`);
+    const response = await axios.get(`${API_URL}/${id}/`);
     return response.data;
   } catch (error) {
     console.error("error fetching post", error);
