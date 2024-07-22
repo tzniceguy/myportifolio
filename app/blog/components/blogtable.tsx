7d97c0897ccc02ba4d43e26552f29167067d5edc
tzniceguy <joeshimbi@gmail.com>
2024-07-22 16:00:41 +0300

    adding blogtable to parent component

---
+++ app/blog/components/blogtable.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { fetchPosts } from "@/app/api/api";
+import { Post } from "@/app/interfaces/post";
+
+export default function BlogTable() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const data = await fetchPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <table className="w-full table-auto text-md  md:text-md text-left rtl:text-right">
+        <thead>
+          <tr>
+            <th>date</th>
+            <th>title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id} className="border-b hover:bg-marshland-500">
+              <td>{post.created_at}</td>
+              <td>
+                <Link href={`blog/post/${post.slug}`}>{post.title}</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
