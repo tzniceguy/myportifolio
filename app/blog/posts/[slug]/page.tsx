96b0c46d968b42e87aab38a069d16035682d4d0c
tzniceguy <joeshimbi@gmail.com>
2024-07-22 16:00:19 +0300

    dynamic rendering of each individaul blogpost

---
+++ app/blog/posts/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import { fetchPost, fetchPosts } from "@/app/api/api";
+import { Post } from "../../../interfaces/post";
+
+export async function generateStaticParams() {
+  const response = await fetchPosts();
+
+  return response.map((post: Post) => ({
+    slug: post.slug,
+  }));
+}
+
+//fetching data for specific post
+async function getPostData(slug: Post) {
+  try {
+    const post = await fetchPost(slug);
+    return post;
+  } catch (error) {
+    return null;
+  }
+}
+
+export default async function Page({ params }) {
+  const { slug } = params;
+  const post = await getPostData(slug);
+
+  return (
+    <article>
+      <h1>{post.title}</h1>
+      <div>{post.body}</div>
+      <div>{post.created_at}</div>
+    </article>
+  );
+}
