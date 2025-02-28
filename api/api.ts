760a4828f9b2bbacee1a3fec0a898078b7120511
tzniceguy <joeshimbi@gmail.com>
2025-02-28 11:04:03 +0300

    api route ignoring

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/";
+const API_URL = "http://192.168.1.162:8000/api/";
 
 export const fetchPosts = async () => {
   try {
@@ -15,4 +15,14 @@ export const fetchPosts = async () => {
 export async function fetchPost(slug: any) {
   const response = await axios.get(`${API_URL}posts/${slug}`);
   return response.data;
+}
+
+export async function createPost(data: any) {
+  try {
+    const response = await axios.post(`${API_URL}posts/`, data);
+    return response.data;
+  } catch (error) {
+    console.error("error creating post:", error);
+    throw error;
+  }
 }
